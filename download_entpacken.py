584c5bfea98cc55e3c02da28769f4a24d916e513
felix <fel.Bachmann@gmail.com>
2021-05-03 00:07:35 +0200

    automated download and unzipping  - no error message if zip file or unzipped folder already exists  - only url has to be passed to download and unzip function

--- download_entpacken.py
+++ download_entpacken.py
@@ -1,13 +1,24 @@
+import os
 import requests
 import zipfile
 
 
-def download(url, path, chunk_size=128):
-    r = requests.get(url, stream=True, allow_redirects=True)
-    with open(path, 'wb') as fd:
-        for chunk in r.iter_content(chunk_size=chunk_size):
-            fd.write(chunk)
+def download(url):
+    zip_name = url[56:]  # name from zip file
+    folder_name = url[56:-4]  # folder name for unzipping
+    wd_path = os.getcwd()  # path from current working dir
+    path = os.path.join(wd_path, folder_name)  # create folder for unzipping
 
-    with zipfile.ZipFile(path, 'r') as zip_ref:
-        unzip_path = path[:-4]
-        zip_ref.extractall(unzip_path)
+    if not os.path.isfile(zip_name):
+        r = requests.get(url, stream=True, allow_redirects=True)
+        with open(zip_name, 'wb') as fd:
+            for chunk in r.iter_content(chunk_size=128):
+                fd.write(chunk)
+    else:
+        print('zip-file exists')
+
+    if not os.path.exists(path):
+        with zipfile.ZipFile(zip_name, 'r') as zip_ref:
+            zip_ref.extractall(path)
+    else:
+        print('zip folder exists')
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import download_entpacken
 import processing_img
 from datetime import datetime
@@ -7,16 +8,13 @@ start_time = datetime.now()
 
 def main():
     url = 'https://upload.uni-jena.de/data/605dfe08b61aa9.92877595/GEO419_Testdatensatz.zip'
-    path = '/home/felix/PycharmProjects/py_scripts_419a/Daten.zip'
 
-    # download_entpacken.download(url, path, chunk_size=128)
-
-    processing_img.open_raster_file(path)
+    download_entpacken.download(url)
+    # processing_img.open_raster_file(path)
 
     end_time = datetime.now()
     print(f"\n end-time =", end_time - start_time, "Hr:min:sec \n")
-    #
-    #
+
 
 # main func
 if __name__ == '__main__':
