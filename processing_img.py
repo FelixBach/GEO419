2bd4b87e9028c42c569cfa60613dbaca220c7814
felix <fel.Bachmann@gmail.com>
2021-05-03 12:07:13 +0200

    small changes

--- processing_img.py
+++ processing_img.py
@@ -1,29 +1,34 @@
+import os
 import numpy as np
 import rasterio as rio
 import glob
 import math
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, image as mpimg
 
 
 def open_raster_file(path):
     file_list = []
-    for name in glob.glob(path[:-4] + str('/') + str('*.tif')):
+    for name in glob.glob(path + str('/*.tif')):
         file_list.append(name)
+        file_list = [w.replace('\\', '/') for w in file_list]
+        print(file_list)
 
-    with rio.open(file_list[0], 'r') as src:
-        ras_meta = src.profile
-        arr = src.read()  # read all raster values
-        print(arr)
-        print('test')
-        arr_mean = np.nanmean(arr)
-        print(arr_mean)
-        arr_db = np.array(arr)
-        arr_db = np.array(list(map(np.float, arr)))
-        print(arr_db)
+    for i, subset in enumerate(file_list):
+        with rio.open(file_list[i]) as src:
+            band_1 = src.read(1)
 
-    ras_meta.update(count=1,
-                    dtype=rio.float32,
-                    nodata=0)
+    #with rio.open(file_list[0], 'r') as src:
+        #ras_meta = src.profile
+        #arr = src.read()  # read all raster values
+        #print(arr)
+        #print('test')
+        #arr_mean = np.nanmean(arr)
+        #print(arr_mean)
+        #arr_db = np.array(arr)
+        #arr_db = np.array(list(map(np.float, arr)))
+        #print(arr_db)
+
+    # ras_meta.update(count=1, dtype=rio.float32, nodata=0)
 
     # with rio.open(path[:-4] + str('/') + str('test_result.tif'), 'w', **ras_meta) as dst:
         # dst.write(arr_mean)
