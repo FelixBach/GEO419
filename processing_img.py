7a8292312af8cd1891715ea12dc7c877048b3830
Felix Bachmann <fel.bachmann@gmail.com>
2021-05-04 17:28:09 +0200

    added function for processing image

--- processing_img.py
+++ processing_img.py
@@ -2,42 +2,51 @@ import os
 import numpy as np
 import rasterio as rio
 import glob
-import math
+import numpy
 from matplotlib import pyplot as plt, image as mpimg
+
+
+def calculation(band_1):
+    db_pixel = []
+    db_res = 10 * numpy.log10(band_1)
+    db_pixel.append(db_res)
+    # print(np.nanmedian(db_pixel))
+    # print(type(db_pixel[0:2]))
+    # for k, value in enumerate(db_pixel):
+    #     if value.any() <= -50:
+    #         db_pixel[k] = -99
+    # print(np.nanmin(db_pixel))
+    # print(max(db_pixel))
+
+    return db_pixel
 
 
 def open_raster_file(path):
     file_list = []
-    for name in glob.glob(path + str('/*.tif')):
+    for i, name in enumerate(glob.glob(path + str('/*.tif'))):
         file_list.append(name)
         file_list = [w.replace('\\', '/') for w in file_list]
+        file_name = file_list[i].rsplit('/', 1)[-1]
         print(file_list)
+        print(file_name)
 
-    for i, subset in enumerate(file_list):
-        with rio.open(file_list[i]) as src:
-            band_1 = src.read(1)
+        for j, file in enumerate(file_list):
+            with rio.open(file_list[i]) as src:
+                band_1 = src.read(1)
+                print(max(band_1))
 
-    #with rio.open(file_list[0], 'r') as src:
-        #ras_meta = src.profile
-        #arr = src.read()  # read all raster values
-        #print(arr)
-        #print('test')
-        #arr_mean = np.nanmean(arr)
-        #print(arr_mean)
-        #arr_db = np.array(arr)
-        #arr_db = np.array(list(map(np.float, arr)))
-        #print(arr_db)
+                np.seterr(divide='ignore', invalid='ignore')
 
-    # ras_meta.update(count=1, dtype=rio.float32, nodata=0)
+                db_pixel = calculation(band_1)
 
-    # with rio.open(path[:-4] + str('/') + str('test_result.tif'), 'w', **ras_meta) as dst:
-        # dst.write(arr_mean)
+                with rio.open(file_list[j]) as src:
+                    ras_data = src.read()
+                    ras_meta = src.profile
+
+                # make any necessary changes to raster properties, e.g.:
+                ras_meta.update(count=1, dtype=rio.float32, nodata=-99)
+
+                with rio.open(file_name, 'w', **ras_meta) as dst:
+                    dst.write(db_pixel[j], 1)
 
 
-    # print(arr.shape)  # this is a 3D numpy array, with dimensions [band, row, col]
-    # print(arr)
-
-    # arr_db = 10 * math.log(arr)
-    # print(arr_db)
-
-    # return print(np.array(file_list[0]))
