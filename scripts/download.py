13f602c98334f6a44f5d764313b991512f4a3730
Felix Bachmann <fel.bachmann@gmail.com>
2021-07-22 16:45:02 +0200

    if download file is not valid a error message is printed an it is possible to type a new URL. Only .zip are supported

--- scripts/download.py
+++ scripts/download.py
@@ -9,15 +9,41 @@ def download(url, path):
     :param path: string: Is the path where the download file and other folders are created and the result is saved
     :return: Function has no return value
     """
-    zip_name = url.rsplit('/', 1)[-1]  # get name after last slash
-    zip_path = os.path.join(path, zip_name)
+    zip_url = url.rsplit('.', 1)[-1]
+    av = requests.head(url).status_code
 
-    if not os.path.isfile(zip_path):
-        print(f'Downloading file')
-        r = requests.get(url, stream=True, allow_redirects=True)
-        with open(zip_path, 'wb') as fd:
-            for chunk in r.iter_content(chunk_size=128):
-                fd.write(chunk)
+    if av == 200 and zip_url == 'zip':
+        print(f'URL is valid.')
 
-    else:
-        print(f'Download file {zip_name} already exists')
+        zip_name = url.rsplit('/', 1)[-1]  # get name after last slash
+        zip_path = os.path.join(path, zip_name)
+
+        if not os.path.isfile(zip_path):
+            print(f'Downloading file')
+            r = requests.get(url, stream=True, allow_redirects=True)
+            with open(zip_path, 'wb') as fd:
+                for chunk in r.iter_content(chunk_size=128):
+                    fd.write(chunk)
+        else:
+            print(f'Download file {zip_name} already exists')
+
+    if av != 200:
+        while av != 200:
+            print(f'URL is not valid. Please enter a new one and hit enter. '
+                  f'Make sure that the url ends on ".zip" otherwise it can not be unziped.')
+            url = input()
+            av = requests.head(url).status_code
+            zip_url = url.rsplit('.', 1)[-1]
+            if av == 200 and zip_url == 'zip':
+                zip_name = url.rsplit('/', 1)[-1]  # get name after last slash
+                zip_path = os.path.join(path, zip_name)
+
+                if not os.path.isfile(zip_path):
+                    print(f'Downloading file')
+                    r = requests.get(url, stream=True, allow_redirects=True)
+                    with open(zip_path, 'wb') as fd:
+                        for chunk in r.iter_content(chunk_size=128):
+                            fd.write(chunk)
+                else:
+                    print(f'Download file {zip_name} already exists')
+                break
