16e7e20efaa1f2ff9e78143c064cecc3eba85c5d
Felix Bachmann <fel.bachmann@gmail.com>
2021-07-22 16:47:05 +0200

    added print statement in main.py

--- main.py
+++ main.py
@@ -13,11 +13,12 @@ def main():
     url = 'https://upload.uni-jena.de/data/60e5d639dd52a0.78161215/GEO419_Testdatensatz.zip'
 
     print(f'Type or copy the path to working directory')
-    path = input()
+    # path = input()
+    path = "/home/felix/Dokumente"
     # path = os.getcwd()
     print(path)
 
-    print(f'Type or copy the URL')
+    print(f'Type or copy the URL. Make sure that the URL ends on ".zip". Otherwise its can not be downloaded or unziped')
     # url = input()
 
     download.download(url, path)
